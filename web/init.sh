949f5eb16f36c35b673c42d263f496f45f89e659
MiTHyber <shevva1304@yahoo.com>
2016-11-10 02:19:21 -0800

    Django in action

--- web/init.sh
+++ web/init.sh
@@ -1,4 +1,5 @@
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-sudo gunicorn -c /home/box/web/etc/gunicorn.conf hello
+sudo gunicorn -c /home/box/web/etc/gunicorn.conf hello --daemon
+sudo gunicorn -c /home/box/web/etc/gunicorn2.conf wsgi --daemon
 sudo /etc/init.d/gunicorn restart
