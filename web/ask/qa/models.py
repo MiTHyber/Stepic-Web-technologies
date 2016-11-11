82f9720d79ef04be28efc88c9c274751c4c19bbd
MiTHyber <shevva1304@yahoo.com>
2016-11-11 02:25:16 -0800

    I'm not sure

--- web/ask/qa/models.py
+++ web/ask/qa/models.py
@@ -1,5 +1,29 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class QuestionManager(models.Manager):
+    def new(self):
+        return self.get_queryset().all().order_by('-added_at')
+
+    def popular(self):
+        return self.get_queryset().all().order_by('-likes')
+
+
+class Question(models.Model):
+    title = models.CharField(max_length=50)
+    text = models.TextField()
+    added_at = models.DateField(auto_now_add=True)
+    rating = models.IntegerField()
+    author = models.ForeignKey(User, related_name='author')
+    likes = models.ManyToManyField(User, related_name='liked')
+    object = QuestionManager()
+
+
+class Answer(models.Model):
+    text = models.TextField()
+    added_at = models.DateField(auto_now_add=True)
+    question = models.ForeignKey(Question)
+    author = models.ForeignKey(User)
--- web/init.sh
+++ web/init.sh
@@ -1,5 +1,11 @@
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-sudo gunicorn -c /home/box/web/etc/gunicorn.conf hello --daemon
-sudo gunicorn -c /home/box/web/etc/gunicorn2.conf wsgi --daemon
+sudo gunicorn -c /home/box/web/etc/gunicorn.conf hello&
+sudo gunicorn -c /home/box/web/etc/gunicorn2.conf wsgi&
 sudo /etc/init.d/gunicorn restart
+sudo /etc/init.d/mysql start
+mysql -u root -e "CREATE USER 'saya'@'localhost'"
+mysql -u root -e "SET PASSWORD FOR 'saya'@'localhost' = PASSWORD('sayanouta')"
+mysql -u root -e "CREATE DATABASE ask"
+mysql -u root -e "GRANT ALL ON ask.* TO 'saya'@'localhost'"
+sudo ./ask/manage.py migrate
